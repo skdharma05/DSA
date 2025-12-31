37a5a6ac245f355dad3d036fe40e651767472874
Dharma S <skdharma05@gmail.com>
2025-12-31 07:54:45 +0530

    Add brute-force solution for minimizing maximum distance between gas stations
    - Implements O(k*n) approach by iteratively placing stations in the longest segment
    - Calculates final maximum distance after all extra stations are added
    - Example usage included for validation

---
+++ Binary Search/BS on Answer/minimiseMaximumDistanceBetweenGasStations.py
@@ -0,0 +1,31 @@
+class MinimiseMaximumDistanceBetweenGasStations:
+    def brute(self, arr, k):
+        n= len(arr)
+        how_many = [0]*(n-1)
+
+        for _ in range(k):
+            max_section = -1
+            max_ind = -1
+
+            for i in range( n-1):
+                diff = arr[i+1]-arr[i]
+                section_length = diff /(how_many[i]+1)
+                if section_length>max_section:
+                    max_section = section_length
+                    max_ind = i
+
+            how_many[max_ind]+=1
+        max_ans = -1
+        for i in range(n-1):
+            diff = arr[i+1]-arr[i]
+            section_length = diff /(how_many[i]+1)
+            max_ans = max(max_ans,section_length)
+        
+        return max_ans
+
+
+arr = [1, 2, 3, 4, 5]
+k = 4
+
+minimiseMaximumDistanceBetweenGasStations = MinimiseMaximumDistanceBetweenGasStations()
+print(minimiseMaximumDistanceBetweenGasStations.brute(arr,k))
