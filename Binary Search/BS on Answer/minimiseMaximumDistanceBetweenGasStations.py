c3805aa8e246d26f9936adae1329b16c4758c560
Dharma S <skdharma05@gmail.com>
2026-01-01 08:06:59 +0530

    Binary Search : Optimize max-distance gas station placement using max-heap

--- Binary Search/BS on Answer/minimiseMaximumDistanceBetweenGasStations.py
+++ Binary Search/BS on Answer/minimiseMaximumDistanceBetweenGasStations.py
@@ -1,3 +1,5 @@
+import heapq
+
 class MinimiseMaximumDistanceBetweenGasStations:
     def brute(self, arr, k):
         n= len(arr)
@@ -22,7 +24,26 @@ class MinimiseMaximumDistanceBetweenGasStations:
             max_ans = max(max_ans,section_length)
         
         return max_ans
+    
 
+    def better(sef,arr,k):
+        n = len(arr)
+        how_many = [0]*(n-1)
+
+        pq =[]
+        for i in range(n-1):
+            dist = arr[i+1]- arr[i]
+            heapq.heappush(pq,(-dist,i))
+        
+        for _ in range(k):
+            negDist , idx = heapq.heappop(pq)
+            how_many[idx]+=1
+            total_dist = arr[idx+1]- arr[idx]
+            newDist = total_dist /(how_many[idx]+1)
+            heapq.heappush(-negDist,idx)
+
+        return pq[0][0]
+    
 
 arr = [1, 2, 3, 4, 5]
 k = 4
