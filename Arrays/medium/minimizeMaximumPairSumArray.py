6599904ec66e8e7272ffaa98b4acf51e9816ed33
Dharma S <skdharma05@gmail.com>
2026-01-24 06:49:11 +0530

    Array: Implement greedy and two-pointer solutions for maximum pair sum minimization

--- Arrays/medium/minimizeMaximumPairSumArray.py
+++ Arrays/medium/minimizeMaximumPairSumArray.py
@@ -6,9 +6,25 @@ class Solution:
             current_sum = nums[i] + nums[len(nums)-1-i] # 1 index + last index
             max_sum =  max(max_sum,current_sum )
         return max_sum
+    
+    def optimal2(self,nums):
+        sorted_array = sorted(nums)
+
+        left = 0
+        right = len(nums)-1
+        max_sum = 0
+
+        while left < right:
+            maxi = sorted_array[left] + sorted_array[right]
+            max_sum = max(max_sum,maxi)
+            left+=1
+            right-=1
+        return max_sum
+
 
 if __name__ == "__main__":
     arr = [1,2,3,4,5,6]
     sol = Solution()
     print(sol.optimal(arr))
+    print(sol.optimal2(arr))
 
